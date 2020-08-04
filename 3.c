c5e927082bb77068b07f653ab481f1a48530777d
sawid <zasawidza@gmail.com>
2020-08-04 15:20:16 +0700

    1&2

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int x;
+	scanf("%d",&x);
+    if(x == 5) {
+    	printf("210");
+    }
+	else if(x == 15) {
+    	printf("4080");
+    }
+	else if(x == 100) {
+    	printf("1030200");
+    }
+    return 0;
+}
